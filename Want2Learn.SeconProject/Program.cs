57bb4a907e2262384c38c4901f874e1ddf9d2d26
Artem Lipinskiy <artem_lipinskiy@mail.ru>
2023-06-19 18:14:12 +0200

    created second project

---
+++ Want2Learn.SeconProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Want2Learn.SeconProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
