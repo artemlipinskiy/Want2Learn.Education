b041f54fbf3fcf73d23c0fe46108b522c470a429
Artem Lipinskiy <artem_lipinskiy@mail.ru>
2023-06-26 17:49:42 +0200

    added conditionals project

---
+++ Want2Learn.Conditionals/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Want2Learn.Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int someNumberOne = 23;
+            int someNumberTwo = 23;
+
+            bool firstCond = true;
+            bool secondCond = false;
+            // >
+            bool thirdCond = someNumberOne < someNumberTwo;
+            // >=
+            bool fourthCond = someNumberOne <= someNumberTwo;
+            // == !=
+            bool fifthCond = someNumberOne == someNumberTwo;
+            bool sixthCond = someNumberOne != someNumberTwo;
+            Console.WriteLine(thirdCond);
+            Console.WriteLine(fourthCond);
+            Console.WriteLine(fifthCond);
+            Console.WriteLine(sixthCond);
+            firstCond = !firstCond;
+            secondCond = !secondCond;
+            Console.WriteLine(firstCond);
+
+            string strOne = "Hello";
+            string strTwo = "hello";
+            //Регистр букв стоит учитывать
+            bool seventhCond = strOne == strTwo;
+            Console.WriteLine(seventhCond);
+
+
+            bool firstOp = firstCond || secondCond;
+            Console.WriteLine(firstOp);
+
+            bool secondOp = firstCond && secondCond;
+            Console.WriteLine(secondOp);
+
+            bool thirdOp = (someNumberOne < someNumberTwo) || (someNumberOne > 20);
+            Console.WriteLine(thirdOp);
+
+            //IF
+            if (someNumberOne < someNumberTwo)
+            {
+                Console.WriteLine("First < Second");
+                Console.WriteLine("First < Second");
+                Console.WriteLine("First < Second");
+                Console.WriteLine("First < Second");
+            }
+
+            if (someNumberOne <= someNumberTwo)
+            {
+                Console.WriteLine("First <= Second");
+            }
+
+            //IF ELSE
+            if (someNumberOne < someNumberTwo)
+            {
+                Console.WriteLine("BYE");
+            }
+            else
+            {
+                Console.WriteLine("HELLO");
+                Console.WriteLine("HELLO");
+            }
+
+            //IF ELSE IF
+            if (someNumberOne < someNumberTwo)
+            {
+                Console.WriteLine("BYE");
+            }
+            else if(someNumberOne == someNumberTwo)
+            {
+                Console.WriteLine("QQQ");
+            }
+
+            int inputedNumber = int.Parse(Console.ReadLine());
+            if (inputedNumber == 10)
+            {
+                Console.WriteLine("Число 10");
+            }
+            if (inputedNumber == 20)
+            {
+                Console.WriteLine("Число 20");
+            }
+            if (inputedNumber == 30)
+            {
+                Console.WriteLine("Число 30");
+            }
+            if (inputedNumber == 40)
+            {
+                Console.WriteLine("Число 40");
+            }
+            if (inputedNumber != 10 && inputedNumber != 20 && inputedNumber != 30 && inputedNumber != 40)
+            {
+                Console.WriteLine("Число не 10, 20, 30 или 40");
+            }
+
+            //SWITCH CASE
+            switch (inputedNumber)
+            {
+                case 10:
+                    Console.WriteLine("Число 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Число 20");
+                    break;
+                case 30:
+                    Console.WriteLine("Число 30");
+                    break;
+                case 40:
+                    Console.WriteLine("Число 40");
+                    break;
+                default:
+                    Console.WriteLine("Число не 10, 20, 30 или 40");
+                    break;
+            }
+
+            //Тернарный оператор
+            string someText = inputedNumber > 10 ? "Больше 10" : "Меньше или = 10";
+
+        }
+    }
+}
--- Want2Learn.SeconProject/Program.cs
+++ Want2Learn.SeconProject/Program.cs
@@ -6,7 +6,23 @@ namespace Want2Learn.SeconProject
     {
         static void Main(string[] args)
         {
+            const int HOURSEINDAY = 24;
+            const int MINUTESINDAY = 60;
+
+            int days = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int result = HOURSEINDAY * MINUTESINDAY * days + MINUTESINDAY * hours + minutes;
             Console.WriteLine("Hello World!");
+
+            char symbol = 'q';
+            int symbolCode = (int)symbol;
+            symbolCode += 1;
+            symbol = (char)symbolCode;
+            Console.WriteLine(symbolCode);
+            Console.WriteLine(symbol);
+            char symbolTwo = Console.ReadKey().KeyChar;
         }
     }
 }
