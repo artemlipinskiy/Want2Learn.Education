5da78c723d23244eac7291ae967a6985be5880fd
Artem Lipinskiy <artem_lipinskiy@mail.ru>
2023-08-31 11:34:27 +0200

    522: created class Person

---
+++ Want2Learn.PullRequests/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Want2Learn.PullRequests
+{
+    class Person
+    {
+        private string fullName;
+        private DateTime birthday;
+
+        public string GetFullName()
+        {
+            return fullName;
+        }
+
+        public DateTime GetBirthday()
+        {
+            return birthday;
+        }
+
+
+        public void SetFullName(string fullName)
+        {
+            if (string.IsNullOrEmpty(fullName))
+            {
+                return;
+            }
+            this.fullName = fullName;
+        }
+
+        public void SetBirthday(DateTime birthday)
+        {
+            if (birthday > DateTime.Now)
+            {
+                return;
+            }
+            this.birthday = birthday;
+        }
+    }
+}
