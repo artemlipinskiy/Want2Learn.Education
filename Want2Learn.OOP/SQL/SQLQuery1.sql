f4cbee01784be98765ecbccfd4bce1e33b6d555f
Artem Lipinskiy <artem_lipinskiy@mail.ru>
2023-10-06 18:22:22 +0200

    added script

---
+++ Want2Learn.OOP/SQL/SQLQuery1.sql
@@ -0,0 +1,42 @@
+CREATE TABLE Owner (
+  Id INT PRIMARY KEY,
+  Fullname VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE City (
+  Id INT PRIMARY KEY,
+  Name VARCHAR(50) NOT NULL,
+  Population INT NOT NULL
+);
+
+CREATE TABLE Street (
+  Id INT PRIMARY KEY,
+  Name VARCHAR(50) NOT NULL,
+  CityId INT NOT NULL,
+  FOREIGN KEY (CityId) REFERENCES City(Id)
+);
+
+CREATE TABLE Building (
+  Id INT PRIMARY KEY,
+  StreetId INT NOT NULL,
+  Number INT NOT NULL,
+  AdditionalName VARCHAR(50),
+  FOREIGN KEY (StreetId) REFERENCES Street(Id)
+);
+
+CREATE TABLE Flat (
+  Id INT PRIMARY KEY,
+  BuildingId INT NOT NULL,
+  Number INT NOT NULL,
+  AdditionalName VARCHAR(50),
+  FOREIGN KEY (BuildingId) REFERENCES Building(Id)
+);
+
+CREATE TABLE OwnerFlat (
+  OwnerId INT NOT NULL,
+  FlatId INT NOT NULL,
+  PurchaseDate DATE NOT NULL,
+  FOREIGN KEY (OwnerId) REFERENCES Owner(Id),
+  FOREIGN KEY (FlatId) REFERENCES Flat(Id),
+  PRIMARY KEY (OwnerId, FlatId)
+);
